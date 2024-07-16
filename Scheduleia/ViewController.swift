f990a8f51f0a3c59006997bb5a7060a2e4d43ac8
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-16 15:15:47 +0530

    added login view

--- Scheduleia/ViewController.swift
+++ Scheduleia/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        signInButton.layer.cornerRadius = signInButton.frame.size.height/3
+        signInButton.clipsToBounds = true
+        
+        registerButton.layer.cornerRadius = registerButton.frame.size.height/3
+        registerButton.clipsToBounds = true
+        
+        animateTitle()
+    }
+    
+    func animateTitle(){
+        titleLabel.text = ""
+        
+        let titleName = "Scheduleia"
+        
+        var index = 1.0;
+        
+        for x in titleName{
+            Timer.scheduledTimer(withTimeInterval: 0.1*index, repeats: false, block: {_ in
+                self.titleLabel.text?.append(x)
+            })
+            
+            index += 1
+        }
     }
 
 
