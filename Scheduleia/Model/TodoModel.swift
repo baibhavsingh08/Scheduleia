aeeed6b399751bdd0792cbeac9f08131b4debfb1
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-18 15:59:21 +0530

    made firebase collection

--- Scheduleia/Model/TodoModel.swift
+++ Scheduleia/Model/TodoModel.swift
@@ -6,17 +6,27 @@
 //
 
 struct TodoModel{
-    var userName: String = "abc"
-    var password: String = "123"
+
     var decription: String = "hii"
     var heading: String = "hello"
     var deadline: String = "12:00"
+    var priority: Int = 0
+    var email: String
     
-    init(userName: String, password: String, decription: String, heading: String, deadline: String) {
-        self.userName = userName
-        self.password = password
+    init(decription: String, heading: String, deadline: String, priority: Int, email: String) {
         self.decription = decription
         self.heading = heading
         self.deadline = deadline
+        self.priority = priority
+        self.email = email
+    }
+    func toDictionary() -> [String: Any] {
+            return [
+                "decription": decription,
+                "heading": heading,
+                "deadline": deadline,
+                "priority": priority,
+                "email": email
+            ]
     }
 }
--- Scheduleia/TodayMainViewController.swift
+++ Scheduleia/TodayMainViewController.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 import FirebaseAuth
-
+import FirebaseFirestore
 
 
 class TodayMainViewController: UIViewController {
     
+    let db = Firestore.firestore()
     var model = [TodoModel]()
+    
+    var textField1: UITextField!
+       var textField2: UITextField!
+       var textField3: UITextField!
+    
+    var vc: UIViewController! = nil
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -29,7 +36,7 @@ class TodayMainViewController: UIViewController {
         super.viewDidLoad()
         
         for _ in 1...10{
-            model.append(TodoModel(userName: "aa", password: "aa", decription: "aa", heading: "aa", deadline: "aa"))
+            model.append(TodoModel(decription: "aa", heading: "aa", deadline: "aa", priority: 1, email: "Asasaa"))
         }
 
         title = "Today's Tasks"
@@ -53,9 +60,6 @@ class TodayMainViewController: UIViewController {
                     button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(50 + tabBarHeight)).isActive = true
                 }
         
-        button.layer.cornerRadius = button.frame.size.width/2
-        button.clipsToBounds = true
-        
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
             
         
@@ -67,19 +71,82 @@ class TodayMainViewController: UIViewController {
     }
     
     @objc func buttonPressed(){
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyBoard.instantiateViewController(identifier: "testVC")
+         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+         vc = storyBoard.instantiateViewController(identifier: "testVC")
         vc.navigationItem.title = "AAA"
         
-        let label = UILabel()
-        label.text = "Hii"
-        label.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
-        vc.view.addSubview(label)
+//        let label = UILabel()
+//        label.text = "Hii"
+//        label.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+//        vc.view.addSubview(label)
+        
+         textField1 = UITextField()
+        textField1.placeholder = "Enter text here"
+        textField1.textColor = .black
+        textField1.backgroundColor = .white
+        textField1.frame = CGRect(x: 100, y: 100, width: 300, height: 60)
+
+        vc.view.addSubview(textField1)
+
+         textField2 = UITextField()
+        textField2.placeholder = "Enter text here"
+        textField2.textColor = .black
+        textField2.backgroundColor = .white
+        textField2.frame = CGRect(x: 100, y: 200, width: 300, height: 60)
+
+        vc.view.addSubview(textField2)
+        
+         textField3 = UITextField()
+        textField3.placeholder = "Enter text here"
+        textField3.textColor = .black
+        textField3.backgroundColor = .white
+        textField3.frame = CGRect(x: 100, y: 400, width: 300, height: 60)
+
+        vc.view.addSubview(textField3)
+        
+        
+        let button = UIButton()
+        button.setTitle("Add Task", for: .normal)
+        button.backgroundColor = .systemBlue
+        
+        button.frame = CGRect(x: 100, y: 600, width: 300, height: 60)
+
+        vc.view.addSubview(button)
+        button.addTarget(self, action: #selector(saveDataToFirebase), for: .touchUpInside)
+
         
         navigationController?.pushViewController(vc, animated: true)
 //        tableView.reloadData()
     }
     
+    func createTextField()->UITextField{
+        let textField = UITextField()
+        textField.placeholder = "Enter text here"
+        textField.textColor = .black
+        textField.backgroundColor = .white
+        return textField
+    }
+    
+    @objc func saveDataToFirebase(){
+        print("hello")
+        
+        
+        if let msgHeading = textField1?.text, let msgDescription = textField2?.text, let msgDeadline = textField3?.text, let msgSender = Auth.auth().currentUser?.email{
+            
+            let msg = TodoModel(decription: msgHeading, heading: msgDescription, deadline: msgDeadline, priority: 1, email: msgSender)
+            let msgData = msg.toDictionary()
+
+            db.collection("todoData").addDocument(data: ["sender": msgSender, "data": msgData], completion: nil)
+            print("in")
+        
+        }else{
+            print("err")
+        }
+        
+        navigationController?.popViewController(animated: true)
+
+        
+    }
 }
 
 
@@ -112,7 +179,7 @@ extension TodayMainViewController: UITableViewDelegate, UITableViewDataSource  {
         cell.headingLabel.text = model[indexPath.row].heading
         cell.deadlineLabel.text = model[indexPath.row].deadline
         
-        cell.colorLabel.backgroundColor = .red
+//        cell.colorLabel.backgroundColor = .red
 //        cell.description = model[indexPath.row].decription
         
         return cell
